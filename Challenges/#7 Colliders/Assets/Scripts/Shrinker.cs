a8eb189fa0d0932455a176ab46d2b5b77aab03ec
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-27 14:54:40 -0300

    Create Shrinker Script

---
+++ Challenges/#7 Colliders/Assets/Scripts/Shrinker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrinker : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 shrinked = new Vector3(0.2f, 0.2f, 0.2f);
+    private Vector3 notShrinked = new Vector3(1f, 1f, 1f);
+    private bool isShrinked = false;
+    private float timer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (Time.time > timer)
+        {
+            if (!isShrinked)
+            {
+                other.transform.localScale = shrinked;
+            }
+            else
+            {
+                other.transform.localScale = notShrinked;
+            }
+
+            isShrinked = !isShrinked;
+        }
+
+        timer = Time.time + 2;
+    }
+}
