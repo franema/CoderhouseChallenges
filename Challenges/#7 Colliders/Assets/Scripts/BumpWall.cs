efc682e4efb4bf37431b385c26042f8d1fa1e245
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-27 14:53:40 -0300

    Create BumpWall Script

---
+++ Challenges/#7 Colliders/Assets/Scripts/BumpWall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpWall : MonoBehaviour
+{
+    private Transform newTransform;
+    private Vector3 newPosition;
+    private Quaternion newRotation;
+    private float timer;
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        timer = Time.time;
+    }
+    void OnCollisionStay(Collision other)
+    {
+        if (Time.time > timer + 2)
+        {
+            newPosition = new Vector3(Random.Range(-30f, 35f), Random.Range(0.1f, 1f), Random.Range(-30f, 35f));
+            newRotation = new Quaternion(Random.Range(0.1f, 180), Random.Range(0.1f, 180), Random.Range(0.1f, 180), Random.Range(0.1f, 180));
+            transform.position = newPosition;
+            transform.rotation = newRotation;
+        }
+    }
+
+}
