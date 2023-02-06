4b0ede54a960b099697f472948b81d290b086c35
DonCosme <109149406+franema@users.noreply.github.com>
2023-02-06 18:26:53 -0300

    Create PlayerMovement Script

--- Challenges/FinalProject/Assets/Script/PlayerMovement.cs
+++ Challenges/FinalProject/Assets/Script/PlayerMovement.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 1;
+    [SerializeField] private float speed = 0;
     private float rotationSpeed = 100;
+
 
     void Update()
     {
         Move();
+        if(!Input.anyKey)
+        {
+            speed = 0;
+        }
     }
 
     public void Move()
     {
         var vertical = Input.GetAxisRaw("Vertical");
         var horizontal = Input.GetAxisRaw("Horizontal");
-
         var rotation = new Vector3(0, horizontal, 0);
+        if(Input.GetKey(KeyCode.LeftShift)) 
+        {
+            speed = 4;
+        } else 
+        {
+            speed = 1;
+        }
 
         transform.position += transform.forward * speed * Time.deltaTime * vertical;
         transform.Rotate(rotation * rotationSpeed * Time.deltaTime);
     }
+
+    
 }
