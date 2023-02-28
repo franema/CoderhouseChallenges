7922a811bd1d81140a3d2334080a10393f58cc8e
DonCosme <109149406+franema@users.noreply.github.com>
2023-02-28 09:58:22 -0300

    Creates Menu Script

---
+++ Challenges/FinalProject/Assets/Script/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] private AudioMixer audioMixer;
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+        Debug.Log(volume);
+    }
+
+    public void BackToMenu ()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+  
+}
