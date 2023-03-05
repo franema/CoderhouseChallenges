4e38876b724147a9b7966b7dcecd1c5041227656
DonCosme <109149406+franema@users.noreply.github.com>
2023-03-05 16:06:05 -0300

    Create Cube Data Scriptable Object

---
+++ Challenges/FinalProject/Assets/Script/ScriptableObjects/CubeData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Cube/CubeData")]
+public class CubeData : ScriptableObject
+{
+    public Vector3 direction;
+    public int inverter;
+    
+}
